a1ebf145a8ba68fdbc0b16ba185b5bf5147912da
kuan <woaini3900098@qq.com>
2019-06-28 15:09:29 +0800

    更新

---
+++ app/Http/Controllers/Index/TopicsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Index;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Topic;
+
+class TopicsController extends Controller
+{
+
+}
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -1,9 +1,21 @@
-    
 <?php
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Category;
+
 class Topic extends Model
 {
-	protected $fillable = [
-        'title', 'body', 'category_id', 'excerpt', 'slug'
+    protected $fillable = [
+        'title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'
     ];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
